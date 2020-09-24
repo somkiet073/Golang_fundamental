92fd075a3ae27b9916f545877e0ab5a71f2a9071
somkiet073 <somkiet073@users.noreply.github.com>
2020-09-25 00:10:30 +0700

    commit

--- interfaces.go
+++ interfaces.go
@@ -5,6 +5,8 @@ import (
 	"math"
 )
 
+// interface คือ การระบุว่า struct นั้นๆ ต้องมี func อะไรบ้าง
+// คล้าย กับ implement class ของภาษา PHP
 type geometry interface {
 	area() float64
 	perim() float64
@@ -18,25 +20,33 @@ type circle struct {
 	radius float64
 }
 
+// add func area() เข้าไปใน rect struct
 func (r rect) area() float64 {
 	return r.width * r.height
 }
 
+// add func perim() เข้าไปใน rect struct
 func (r rect) perim() float64 {
 	return 2*r.width + 2*r.height
 }
 
+// add func area() เข้าไปใน circle struct
 func (c circle) area() float64 {
 	return math.Pi * c.radius * c.radius
 }
 
+// add func perim() เข้าไปใน circle struct
 func (c circle) perim() float64 {
 	return 2 * math.Pi * c.radius
 }
 
+// func call geometry interface
 func mesure(g geometry) {
+	// print ดูค่า ของ Struct
 	fmt.Println(g)
+	// print ดูค่า ของ func area ใน struct นั้นๆ
 	fmt.Println(g.area())
+	// print ดูค่า ของ func perim ใน struct นั้นๆ
 	fmt.Println(g.perim())
 }
 
